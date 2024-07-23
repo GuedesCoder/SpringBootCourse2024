33574b1d0cae63ad8367ee943a60a58878c5e6f1
GuedesCoder <leandro.costa30@fatec.sp.gov.br>
2024-07-23 20:38:00 -0300

    Project Created

---
+++ src/main/java/com/guedes/spring/course/GuedesSpringCourseApplication.java
@@ -0,0 +1,13 @@
+package com.guedes.spring.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuedesSpringCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuedesSpringCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guedes/spring/course/GuedesSpringCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guedes.spring.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuedesSpringCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
