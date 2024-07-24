f8b274e4a3b53992aca46299479343c5f2ad230a
GuedesCoder <leandro.costa30@fatec.sp.gov.br>
2024-07-24 00:02:00 -0300

    Service Layer & Component Registration

---
+++ src/main/java/com/guedes/spring/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.guedes.spring.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guedes.spring.course.entities.User;
+import com.guedes.spring.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
