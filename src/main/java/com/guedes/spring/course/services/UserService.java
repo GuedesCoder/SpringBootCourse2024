afbf5b16a8a87078632656cc6dcbc62c3d88a4c0
GuedesCoder <leandro.costa30@fatec.sp.gov.br>
2024-07-28 12:06:07 -0300

    Exception handling - update

--- src/main/java/com/guedes/spring/course/services/UserService.java
+++ src/main/java/com/guedes/spring/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.guedes.spring.course.repositories.UserRepository;
 import com.guedes.spring.course.services.exceptions.DatabaseException;
 import com.guedes.spring.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -42,12 +44,16 @@ public class UserService {
 		} else {
 			throw new ResourceNotFoundException(id);
 		}
-	} 
+	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
